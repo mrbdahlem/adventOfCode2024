610c2c1a547a86dcf859bb6c492c9c6eb43de89c
Brian Dahlem <dahlem.brian@cusd80.com>
2024-12-05 09:31:32 -0700

    Starting Day 5

---
+++ 05.py
@@ -0,0 +1,19 @@
+import helper
+
+day = int(__file__.split("\\")[-1].split(".")[0])
+print ("Day", day)
+
+data = helper.load_data(day)
+# data = helper.load_data(f"{day:02}-samp")
+
+ans = 0
+
+
+
+print("Part 1: ", ans)
+
+ans2 = 0
+
+
+
+print("Part 2: ", ans2)
